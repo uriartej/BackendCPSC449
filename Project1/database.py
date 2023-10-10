9dba68d47cb7817b681b4355a242b470cb3cacab
Juan Uriarte <99374424+uriartej@users.noreply.github.com>
2023-10-09 17:24:40 -0700

    Delete Project1 directory

--- Project1/database.py
+++
@@ -1,7 +0,0 @@
-import urllib.parse
-
-
-database_file_path = "/home/juanuriarte/Project1/Updated Database Project 1.db"
-
-encoded_file_path = urllib.parse.quote(database_file_path, safe = "/:")
-DATABASE_URL = f"sqlite:///{encoded_file_path}"
