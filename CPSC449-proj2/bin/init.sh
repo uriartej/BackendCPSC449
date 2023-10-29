0b8aff28a376518d20412fa63f39f61d735ca796
Juan Uriarte <99374424+uriartej@users.noreply.github.com>
2023-10-29 09:45:45 -0700

    Delete CPSC449-proj2 directory

--- CPSC449-proj2/bin/init.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/sh
-
-./share/users/user_schema_init.py
-./share/enroll/schema_init.py
-
--- CPSC449-proj2/bin/post.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/sh
-
-http --verbose POST localhost:5400/auth_api/books/ @"$1"
-
-# Put in port to KrakenD; Not Finished
